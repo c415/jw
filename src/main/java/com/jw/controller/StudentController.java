42f0bead48d1c7a8ab585f041ae608e679407c66
panpan <17854238041@163.com>
2017-11-21 20:35:40 +0800

    学生登录功能的实现

---
+++ src/main/java/com/jw/controller/StudentController.java
@@ -0,0 +1,45 @@
+package com.jw.controller;
+
+
+import com.jw.pojo.CourseCustom;
+import com.jw.pojo.PagingVO;
+import com.jw.service.CourseService;
+import com.jw.service.StudentService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/student")
+public class StudentController {
+
+    @Resource(name = "courseServiceImpl")
+    private CourseService courseService;
+
+    @Resource(name = "studentServiceImpl")
+    private StudentService studentService;
+    @RequestMapping(value = "/showCourse")
+    public String stuCourseShow(Model model, Integer page) throws Exception {
+
+        List<CourseCustom> list = null;
+        //页码对象
+        PagingVO pagingVO = new PagingVO();
+        //设置总页数
+        pagingVO.setTotalCount(courseService.getCountCouse());
+        if (page == null || page == 0) {
+            pagingVO.setToPageNo(1);
+            list = courseService.findByPaging(1);
+        } else {
+            pagingVO.setToPageNo(page);
+            list = courseService.findByPaging(page);
+        }
+
+        model.addAttribute("courseList", list);
+        model.addAttribute("pagingVO", pagingVO);
+
+        return "student/showCourse";
+    }
+}
