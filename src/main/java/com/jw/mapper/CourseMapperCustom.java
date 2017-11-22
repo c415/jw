79ff27bf4aa14742900a60df012936f11e4a8ca3
panpan <17854238041@163.com>
2017-11-22 10:52:24 +0800

    “学生登录功能的补充

---
+++ src/main/java/com/jw/mapper/CourseMapperCustom.java
@@ -0,0 +1,14 @@
+package com.jw.mapper;
+
+import com.jw.pojo.CourseCustom;
+import com.jw.pojo.PagingVO;
+
+import java.util.List;
+
+
+public interface CourseMapperCustom {
+
+    //分页查询学生信息
+    List<CourseCustom> findByPaging(PagingVO pagingVO) throws Exception;
+
+}
