e2c0a51e44a72629f85612f3487edf74df3b9e15
AimPanpan <31302847+AimPanpan@users.noreply.github.com>
2017-11-23 10:49:04 +0800

    Update SelectedCourseService.java

--- src/main/java/com/jw/service/SelectedCourseService.java
+++ src/main/java/com/jw/service/SelectedCourseService.java
@@ -11,29 +11,17 @@ import java.util.List;
  */
 public interface SelectedCourseService {
 
-    //根据课程ID查询课程
-    List<SelectedCourseCustom> findByCourseID(Integer id) throws Exception;
+   
 
-    //根据课程id分页查询课程
-    List<SelectedCourseCustom> findByCourseIDPaging(Integer page, Integer id) throws Exception;
 
-    //获取该课程学生数
-    Integer countByCourseID(Integer id) throws Exception;
 
-    //查询指定学生成绩
-    SelectedCourseCustom findOne(SelectedCourseCustom selectedCourseCustom) throws Exception;
 
-    //打分
-    void updataOne(SelectedCourseCustom selectedCourseCustom) throws Exception;
 
     //选课
     void save(SelectedCourseCustom selectedCourseCustom) throws Exception;
 
-    //根据学生id查找课程
-    List<SelectedCourseCustom> findByStudentID(Integer id) throws Exception;
 
-    //退课
-    void remove(SelectedCourseCustom selectedCourseCustom) throws Exception;
+
 
 
 
